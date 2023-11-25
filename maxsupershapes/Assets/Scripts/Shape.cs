bae7a62a4027d06851c99464f2921be739918cf4
max-nguyen1 <126340837+max-nguyen1@users.noreply.github.com>
2023-11-25 12:02:00 -0500

    11/25/23

---
+++ maxsupershapes/Assets/Scripts/Shape.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Security.Cryptography;
+using UnityEngine;
+
+public class Shape : MonoBehaviour
+{
+
+    [Header("Rigidbody Object")]
+    public Rigidbody2D rb;
+    [Header("Default Shrinking Speed")]
+    public  float shrinkSpeed = 3f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb.rotation = UnityEngine.Random.Range(0f, 360f);
+        transform.localScale = Vector3.one * 10f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.localScale -= Vector3.one * shrinkSpeed * Time.deltaTime;
+
+        if (transform.localScale.x <= .05f)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
